446be2f990b5884e55b331073ce4361fcd69b713
daniel-fer33 <daniel-fernandez@cookpad.com>
2024-12-20 10:04:32 +0000

    Remove DeepSpeed

--- promptitude/llms/__init__.py
+++ promptitude/llms/__init__.py
@@ -3,7 +3,6 @@ from ._anthropic import Anthropic
 from ._transformers import Transformers
 from ._mock import Mock
 from ._llm import LLM, LLMSession, SyncSession
-from ._deep_speed import DeepSpeed
 from . import transformers
 from . import caches
 
