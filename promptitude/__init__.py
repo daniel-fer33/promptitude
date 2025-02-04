546e412ab734aa695d21c5a6874305e2e2c8700d
daniel-fer33 <daniel-fernandez@cookpad.com>
2025-02-04 19:09:40 +0000

    Enable/disable pyparsing caching (it is not thread safe)

--- promptitude/__init__.py
+++ promptitude/__init__.py
@@ -2,3 +2,7 @@ __version__ = "0.0.0"
 
 from .guidance import Program, ProgramState
 from ._variable_stack import VariableStack
+from ._grammar import enable_pyparsing_cache, disable_pyparsing_cache
+
+
+enable_pyparsing_cache()
